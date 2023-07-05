c545f8ed9809e940b692f6b97ca2655b0cf58607
AKAVespaET3 <quattrini244@gmail.com>
2023-07-05 18:22:04 +0900

    2023-07-05

---
+++ docs/browser_js/js/events.js
@@ -0,0 +1,8 @@
+let keydownObject = document.querySelector("#keydownEnter");
+
+keydownObject.addEventListener('keydown', (event) => {
+    if(event.code == 'Enter'){
+        console.log(`key down: ${event.code}`);    
+    }
+});
+
